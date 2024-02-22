ad21daf828ff18e9a0a6d6611b294e0c1c77b09e
olivierpoirier <117477661+olivierpoirier@users.noreply.github.com>
2024-02-22 11:13:25 -0500

    Changements Oli v1

--- backend/src/main/java/com/swapit/repositories/TotoRepository.java
+++ backend/src/main/java/com/swapit/repositories/TotoRepository.java
@@ -4,4 +4,5 @@ import com.swapit.model.Toto;
 import org.springframework.data.jpa.repository.JpaRepository;
 
 public interface TotoRepository extends JpaRepository<Toto, Integer> {
-}
+
+    //allo
