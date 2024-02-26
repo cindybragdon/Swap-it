629a2c9e9a5d427307ced5996d9976b5b4e42251
cindy <cindy.bragdon@gmail.com>
2024-02-26 11:00:09 -0500

    shadow updated

--- backend/src/main/java/com/swapit/repositories/ShadowRepository.java
+++ backend/src/main/java/com/swapit/repositories/ShadowRepository.java
@@ -9,10 +9,10 @@ public interface ShadowRepository extends JpaRepository<Shadow, Integer> {
 
 
     //Verifies if the email / password combination exists for account connection or forgot pwd (select)
-    public Shadow findByIdUser_UserEmailAndUserPassword(String email,String password);
 
     //Verifies if the email / phone combination exists for 2FA
     public Shadow findByIdUser_UserPhoneAndUserPassword(String phone, String password);
+    public Shadow findByIdUser_UserEmailAndUserPassword(String email,String password);
 
     public Shadow findByIdUser(Integer idUser);
 }
