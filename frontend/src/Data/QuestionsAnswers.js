4a973c579423b4f2c0be06e973a3ec2988354362
cindy <cindy.bragdon@gmail.com>
2024-03-14 19:07:51 -0400

    Page FAQ terminée

---
+++ frontend/src/Data/QuestionsAnswers.js
@@ -0,0 +1,19 @@
+const questionsAnswers = [
+    {
+        itemId: "1",
+        question: "Comment savoir qui j'ai pigé?",
+        answer: "Dans votre compte, cliquez sur 'Voir qui j'ai pigé'. Regardez bien autour de vous pour vous assurer d'être seul et appuyez sur la touche 'Surprise'."
+    },
+    {
+        itemId: "2",
+        question: "Comment savoir si une certaine collègue du bureau sera présente à la pige?",
+        answer: "Allez dans la page de la pige Pige du bureau, puis cliquez sur la liste des membres.  Je croise les doigts avec vous... alors, est-ce qu'elle y va?"
+    },
+    {
+        itemId: "3",
+        question: "Cet été, durant un beach party autour de la piscine et quelques verres de sangria, Marcel m'a avoué qu'il voulait un costume de bain une pièce vert lime. Il ne l'a pas mis dans sa liste de suggestions. Comment puis-je l'ajouter sans qu'il ne s'en rende compte?",
+        answer: "C'est très simple, suivez ces quelques étapes. Rendez-vous dans la pige que vous partagez avec ce brave Marcel. Cliquez sur liste des membres, puis sur Marcel lui-même. Vous verrez ainsi sa liste de suggestions. Appuyez sur Ajouter une suggestion pour Marcel et insérez la nouvelle suggestion. Au fait... pourriez-vous nous envoyer une photo de Marcel dans son nouveau maillot?"
+    }
+]
+
+export default questionsAnswers;
--- frontend/src/pages/FAQ.js
+++ frontend/src/pages/FAQ.js
@@ -1,14 +1,51 @@
-import React from 'react';
+import React, { useState } from 'react';
+import questionsAnswers from "../Data/QuestionsAnswers";
 
-function FAQ() {
-  return (
-    <div className='hero'>
-        <div className='container'>
-            <h1>Page FAQ</h1>
-            <p> Lorem ipsum dolor sit amet.</p>
+const FAQ = () => {
+    const [openItemId, setOpenItemId] = useState(null);
+
+    const toggleItem = (itemId) => {
+        if (openItemId === itemId) {
+            setOpenItemId(null); // Close the item if it is already open
+        } else {
+            setOpenItemId(itemId); // Open the item and close others
+        }
+    };
+
+
+    return (
+        <div className='hero oui'>
+            <div className='container'>
+                <h1>Page FAQ</h1>
+                <p>Parce que parfois, tout comprendre seul, c'est pas un cadeau!</p>
+                <div className="accordion" id="accordionExample">
+                    {questionsAnswers.map((questionAns) => (
+                        <div className="card" key={questionAns.itemId}>
+                            <div className={`card-header ${questionAns.itemId === openItemId ? '' : 'collapsed'}`} id={`heading${questionAns.itemId}`}>
+                                <h2 className="mb-0">
+                                    <button
+                                        className="btn btn-link text-info"
+                                        type="button"
+                                        onClick={() => toggleItem(questionAns.itemId)}
+                                        aria-expanded={openItemId === questionAns.itemId}
+                                        aria-controls={`collapse${questionAns.itemId}`}
+                                    >
+                                        {`Question ${questionAns.itemId} : ${questionAns.question}`}
+                                    </button>
+                                </h2>
+                            </div>
+
+                            <div id={`collapse${questionAns.itemId}`} className={`collapse ${openItemId === questionAns.itemId ? 'show' : ''}`} aria-labelledby={`heading${questionAns.itemId}`}>
+                                <div className="card-body">
+                                    {questionAns.answer}
+                                </div>
+                            </div>
+                        </div>
+                    ))}
+                </div>
+            </div>
         </div>
-    </div>
-  );
-}
+    );
+};
 
 export default FAQ;
