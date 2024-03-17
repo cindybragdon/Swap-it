e3937e836f2e5cf0d6bc698cb869b6b4a3377f47
cindy <cindy.bragdon@gmail.com>
2024-03-16 23:13:24 -0400

    Page MyAccount

--- frontend/src/pages/MyAccount.js
+++ frontend/src/pages/MyAccount.js
@@ -4,15 +4,13 @@ import ImageAccount from "../images/account.jpg";
 const MyAccount = () => {
 
 
-    return (
-        <div className="hero oui">
+    return (<div className="hero oui">
             <div className="container mt-4 mb-4 p-3 d-flex justify-content-center">
                 <div className="card p-4">
                     <div className="image d-flex flex-column justify-content-center align-items-center">
                         <button className="btn btn-secondary">
                             <img src={ImageAccount} height="200" width="250" alt="Account"/>
                         </button>
-                        {/* Corrected the placement of className and used {} to evaluate method calls */}
                         <p className="name mt-3">PRENOM : user.getPrenom *** NOM : user.getNom</p>
 
                         <div className="text mt-3">
@@ -24,16 +22,19 @@ const MyAccount = () => {
                             <p>EMAIL : user.getEmail</p>
                         </div>
 
-                        <div className="col-sm-20 text-center">
+                        <div className="button-container gap-2 d-md-flex justify-content-md-start">
                             <a href='/UpdateAccount'>
                                 <button type="submit" className="btn btn-info w-30">Modifier mon compte</button>
                             </a>
+                            <a href='/piges'>
+                                <button type="submit" className="btn btn-info w-30">Voir mes piges</button>
+                            </a>
                         </div>
+
                     </div>
                 </div>
             </div>
-        </div>
-    );
+        </div>);
 };
 
 export default MyAccount;
