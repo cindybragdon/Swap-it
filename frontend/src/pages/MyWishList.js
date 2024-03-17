863cb7c4b9620c06c0253150bf1a997eb3ade10e
cindy <cindy.bragdon@gmail.com>
2024-03-17 18:45:55 -0400

    Pages MyWishList & AddWishItem

--- frontend/src/pages/MyWishList.js
+++ frontend/src/pages/MyWishList.js
@@ -5,12 +5,10 @@ import ImageRobe from '../images/simons.jpg';
 import ImageVernis from '../images/vernis.jpg';
 import ImageCadeau from '../images/gift.png';
 import {useNavigate} from "react-router-dom";
+import '../MyWishList.css';
 
-
-
-// PROBLÈME DE CSS les cards devraient rester toutes de la meme grosseur malgre les formats d'images differents
+//Les cartes passent sous la navbar et sur le footer.
 // CETTE PAGE CONTIENT DES DONNEES HARDCODEES QUI DOIVENT ETRE REMPLACEES
-// incapable de faire ouvrir le lien cliqué dans un NOUVEL ONGLET
 // Il doit y avoir autant de carte que de suggestions, PLUS une carte qui permet dajouter une suggestion supplementaire
 // L'usager ne doit voir que les suggestions qu'il aura placé LUI-MÊME
 
@@ -25,19 +23,20 @@ const MyWishList = () => {
     }
 
     return (
-        <div className='hero oui'>
+        <div className='hero '>
             <div className="container">
                 <p> getUserName, voici votre liste de suggestions de cadeaux pour la pige getPigeName </p>
                 <div className="row row-cols-1 row-cols-md-3 g-4">
                     <div className="col">
                         <div className="card h-500">
-                            <img src={ImageSouliers} className="card-img-top" alt="..." height="400" width="300"/>
+                            <img src={ImageSouliers} className="card-img-top " alt="..." height="300" width="100"/>
                             <div className="card-body">
                                 <h5 className="card-title">Souliers Nike</h5>
                                 <p className="card-text">J'aimerais avoir ces souliers grandeur 7.5 Mais je crois que ça
                                     dépasse le budjet de la pige.</p>
                                 <a href="https://www.nike.com/ca/fr/u/custom-nike-blazer-mid-77-by-you-10001251/1709858248900"
-                                   className="stretched-link new-tab target='_blank'">Voir cet item en ligne</a>
+                                   className="stretched-link new-tab" target='_blank'>Voir cet item en ligne</a>
+
                             </div>
                         </div>
                     </div>
@@ -48,7 +47,7 @@ const MyWishList = () => {
                                 <h5 className="card-title">Robe Simons</h5>
                                 <p className="card-text">Cette robe est très jolie.</p>
                                 <a href="https://www.simons.ca/fr/vetements-femme/robes/longues/la-longue-robe-col-bateau-dentelle-extensible--11318-216810?catId=7076&colourId=11"
-                                   className="stretched-link new-tab target='_blank'">Voir cet item en ligne</a>
+                                   className="stretched-link new-tab" target='_blank'>Voir cet item en ligne</a>
 
                             </div>
                         </div>
@@ -60,7 +59,7 @@ const MyWishList = () => {
                                 <h5 className="card-title">Vernis a ongles</h5>
                                 <p className="card-text">Délicieuse couleur parfaite pour le printemps.</p>
                                 <a href="https://www.sallyhansen.com/en-us/nail-color/nail-color/pure?shade=peony-origins"
-                                   className="stretched-link new-tab target='_blank'">Voir cet item en ligne</a>
+                                   className="stretched-link new-tab" target='_blank'>Voir cet item en ligne</a>
                             </div>
                         </div>
                     </div>
