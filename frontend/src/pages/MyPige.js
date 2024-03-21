ab6d10e116415fe56106595e007ca3c41fed0ce9
KarolannMauger <karolannmauger@gmail.com>
2024-03-21 09:36:06 -0400

    Ajout de bouton pour aller vers MyWishList, voir commentaire dan MyPige.js

--- frontend/src/pages/MyPige.js
+++ frontend/src/pages/MyPige.js
@@ -1,11 +1,32 @@
-import React from 'react';
-import {useLocation} from "react-router-dom";
+import React, {useState} from 'react';
+import {useLocation, useNavigate} from "react-router-dom";
 
 
 const MyPige = () => {
+    /*
+    * @KarolannMauger
+    * @Date : 2024-03-21
+    * @Revision1 :
+    * Petit problème pour get le idUserPige dans ma list, j'essaie de trouver le bug, voir avec moi pour explication.
+     */
+
+    const navigate = useNavigate();
 
     const location = useLocation();
     const selectedPige = location.state.selectedPige;
+
+    const [idUserPige, setIdUserPige] = useState('');
+
+
+
+    const handleButtonClick = (selectedPige) => {
+        navigate(`/piges/${selectedPige.pige.pigeName}/myWishList`, {state: selectedPige});
+        console.log(selectedPige);
+    }
+    const handleButton2Click = () => {
+        navigate(`/piges`);
+    }
+
 
     return (
         <div className='hero oui'>
@@ -16,6 +37,12 @@ const MyPige = () => {
                 <h1>{selectedPige.user.userFirstName}</h1>
             </div>
             {console.log(selectedPige)}
+            <div className="container">
+                <button onClick={() => handleButtonClick(selectedPige)}> My Wish List </button>
+            </div>
+            <div className="container">
+                <button onClick={() => handleButton2Click()}> My Piges </button>
+            </div>
         </div>
 
     );
