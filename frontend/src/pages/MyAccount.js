11b318e92b8d79f56c5d86373b917911354f0537
cindy <cindy.bragdon@gmail.com>
2024-04-16 16:22:29 -0400

    myAccount

--- frontend/src/pages/MyAccount.js
+++ frontend/src/pages/MyAccount.js
@@ -1,20 +1,32 @@
 import React from 'react';
-import ImageAccount from "../images/Profil.jpg";
+import ImageAccount from "../images/BGMyAccount.jpg";
+import ImageNoImage from "../images/userIcone.jpg"
 
 const MyAccount = () => {
+
+    var fullBackgroundStyle = {
+        backgroundImage: `url(${ImageAccount})`,
+        width: '100vw',
+        minHeight: '100vh',
+        backgroundPosition: 'center',
+        backgroundSize: 'cover',
+        backgroundRepeat: 'no-repeat',
+        padding: 0,
+        margin: 0
+    };
 
     const currentUser = JSON.parse(sessionStorage.user);
     console.log(currentUser.userImage);
     return (
-        <div className="container-account" style={{ backgroundColor: '#009BC1' }}>
+        <div className="container-account" style={fullBackgroundStyle}>
             <div className="container  mb-4 d-flex justify-content-center"  >
-                <div className="card p-4" style={{ backgroundColor: '#FFFCD7' }} id="account-box">
+                <div className="card p-4" style={{ backgroundColor: 'white' }} id="account-box">
                     <div className="image d-flex flex-column justify-content-center align-items-center">
                         <button className="btn btn-secondary">
                             {currentUser.userImage ? <img
                                 src={currentUser.userImage}
                                 height="300" width="375" alt="Account"/> : <img
-                                src={ImageAccount} height="300" width="375" alt="Account"/>}
+                                src={ImageNoImage} height="200" width="275" alt="Account"/>}
 
                         </button>
                         <p className="name mt-3">PRENOM : {currentUser.userFirstName} NOM : {currentUser.userLastName} </p>
--- frontend/src/pages/MyWishList.js
+++ frontend/src/pages/MyWishList.js
@@ -4,7 +4,6 @@ import ImageAdItem from "../images/Listes.jpg";
 import ImageWishedItemBlank from "../images/noWishedItemImage.jpg";
 import { useLocation, useNavigate } from "react-router-dom";
 import BackToTopButton from "../components/BackToTopButton";
-
 const MyWishList = () => {
     var fullBackgroundStyle = {
         backgroundImage: `url(${ImageAdItem})`,
@@ -16,12 +15,10 @@ const MyWishList = () => {
         padding: 0,
         margin: 0
     };
-
     const navigate = useNavigate();
     const location = useLocation();
     const userPige = location.state;
     const [listWishedItems, setListWishedItems] = useState([]);
-
     useEffect(() => {
         const url = `http://localhost:9281/api/getAllWishedItemsFromUserPige?idUserPige=${userPige.idUserPige}`;
         const fetchData = async () => {
@@ -35,26 +32,23 @@ const MyWishList = () => {
         };
         fetchData();
     }, []);
-
     const handleClickToAddItem = () => {
         sessionStorage.setItem('userPigeToAddItem', JSON.stringify(userPige));
         navigate('/pige/myWishList/addWish');
     };
-
     const handleClickBackToPige = () => {
         navigate(`/piges/${userPige.pige.pigeName}`, {state: userPige});
     };
-
     return (
         <div style={fullBackgroundStyle}>
             <div className='container-fluid text-center'>
                 <div className="pt-3">
-                    <h2 className="title-piges" style={{color: '#FF3991'}}>{userPige.user.userFirstName}, voici votre
-                        liste de suggestions de cadeaux pour la pige {userPige.pige.pigeName}
+                    <h2 className="title-piges" style={{color: '#FF3991'}}>Voici la liste de suggestions de cadeaux de {userPige.user.userFirstName}, pour la pige
+                         {userPige.pige.pigeName}
                         <h5><a className="link-opacity-75" onClick={handleClickBackToPige}>Retour à la pige</a></h5>
                     </h2>
                 </div>
-                <div className="row row-cols-1 row-cols-md-4 g-5"> {/* Augmentation de l'espacement avec g-5 */}
+                <div className="row row-cols-1 row-cols-md-4 g-5">
                     <div className="col pb-5">
                         <div className="card card-custom" onClick={handleClickToAddItem}>
                             <img src={ImageCadeau} className="card-image img-fluid"/>
@@ -66,8 +60,8 @@ const MyWishList = () => {
                     </div>
                     {listWishedItems.map((wishedItem, index) => (
                         <div className="col mb-4"
-                             key={index}> {/* mb-4 ajoute un margin-bottom pour séparer verticalement */}
-                            <div className="card card-custom shadow-sm"> {/* shadow-sm pour un effet subtil d'ombre */}
+                             key={index}>
+                            <div className="card card-custom shadow-sm">
                                 <div
                                     className="title-card-piges card-header bg-danger">{wishedItem.wishedItemName}</div>
                                 {wishedItem.wishedItemImage ?
@@ -90,10 +84,10 @@ const MyWishList = () => {
                     ))}
                 </div>
             </div>
-
             <BackToTopButton/>
         </div>
     )
         ;
 }
 export default MyWishList;
+
