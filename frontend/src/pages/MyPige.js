34f596f25a6520769448bc9392f9fe0d77a102dc
olivierpoirier <117477661+olivierpoirier@users.noreply.github.com>
2024-04-18 08:25:59 -0400

    Pas afficher invitiation dans mypige si la personne a répondue

--- frontend/src/pages/MyPige.js
+++ frontend/src/pages/MyPige.js
@@ -201,15 +201,16 @@ const MyPige = () => {
                             <div className="card-header" style={{backgroundColor: '#808080'}}>
                                 <h3>N'ont pas encore répondu : </h3>
                             </div>
-                            {listInvitation.map(inv =>
-                                <div className="card m-1" style={{backgroundColor: '#C0C0C0'}}>
+                            {listInvitation.map((inv, index) =>
+                                !inv.asBeenAnswered ?
+                                <div key={index} className="card m-1" style={{backgroundColor: '#C0C0C0'}}>
                                     <div className="container row border border-2 p-2 ">
                                         <h6 className="card-title">
                                             Nom : {inv.firstNameOfWantedUser} {inv.lastNameOfWantedUser}
                                             Email : {inv.emailWantedUser}</h6>
 
                                     </div>
-                                </div>
+                                </div> : ''
                             )}
 
                         </div>
