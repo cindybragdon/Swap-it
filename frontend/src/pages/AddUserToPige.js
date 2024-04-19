23cb08fe744dacdcbdbbe8128f09571fb95a1598
cindy <cindy.bragdon@gmail.com>
2024-04-19 15:32:19 -0400

    Style un peu partout

--- frontend/src/pages/AddUserToPige.js
+++ frontend/src/pages/AddUserToPige.js
@@ -1,15 +1,37 @@
-import React, {useState} from "react";
-import ImageLapin from "../images/Lapin.jpg";
-import ImageChapeauLapin from "../images/ChapeauLapins.jpg";
-import {useNavigate} from "react-router-dom";
+import React, { useState } from "react";
+import ImageLapin from "../images/AddPersonneFleur.jpg";
+import ImageChapeauLapin from "../images/AddPersonneBouquet.jpg";
+import ImageBF from "../images/AddPersonneBG.jpg";
+import { useNavigate } from "react-router-dom";
 import axios from "axios";
-
 
 const AddUserToPige = () => {
 
+    var sectionStyle = {
+        backgroundImage: `url(${ImageBF})`,
+        backgroundSize: 'cover',
+        backgroundPosition: 'center',
+        backgroundRepeat: 'no-repeat',
+        height: '100vh',
+        justifyContent: 'space-around',
+        fontFamily: "Reddit Mono",
+        width: '100vw'
+    };
+
+    const cardStyle = {
+        width: '48%',
+        height: 'auto',
+        display: 'flex',
+        flexDirection: 'column',
+        justifyContent: 'space-between'
+    };
+
+    const imageStyle = {
+        height: '180px',
+        width: '210px'
+    };
 
     const [listFormInv, setListFormInv] = useState([]);
-
     const [emailToAdd, setEmailToAdd] = useState('');
     const [firstNameToAdd, setFirstNameToAdd] = useState('');
     const [lastNameToAdd, setLastNameToAdd] = useState('');
@@ -17,49 +39,41 @@ const AddUserToPige = () => {
     const navigate = useNavigate();
 
     const formInv = {
-
-        pige:JSON.parse(sessionStorage.pigeToAddPeopleTo),
+        pige: JSON.parse(sessionStorage.pigeToAddPeopleTo),
         firstNameOfWantedUser: firstNameToAdd,
-        lastNameOfWantedUser:lastNameToAdd,
+        lastNameOfWantedUser: lastNameToAdd,
         emailWantedUser: emailToAdd,
-        asBeenAnswered:asBeenAnswered
-    }
+        asBeenAnswered: asBeenAnswered
+    };
 
     const handleSubmit = (event) => {
         setListFormInv(listFormInv => [...listFormInv, formInv]);
         console.log(listFormInv);
         event.preventDefault();
-    }
-
-
-
-
-
+    };
 
     const onClickCreateInvitations = () => {
-
         const url = `http://localhost:9281/api/createInvitation`;
         axios.post(url, listFormInv)
-            .then(res => {if(res.data === "ACK-400") {
-                alert(`Les invitations ont bien été envoyées!`) ;
-                navigate(`/piges`);
-
-            } else {
-                alert(`Erreur : Les invitations n'ont pas été envoyées.`)
-            }})
+            .then(res => {
+                if (res.data === "ACK-400") {
+                    alert(`Les invitations ont bien été envoyées!`);
+                    navigate(`/piges`);
+                } else {
+                    alert(`Erreur : Les invitations n'ont pas été envoyées.`)
+                }
+            })
             .catch(err => console.log(err));
-
-    }
+    };
 
     return (
-        <div className="container mb-4 p-3 d-flex">
-
-            <div className="card text-center " id="container-forgot">
-                <div className="card-header h5 text-white bg-info">On ajoute des participants au chapeau!</div>
+        <div className="container mb-4 p-3 d-flex" style={sectionStyle}>
+            <div className="card text-center" style={cardStyle} id="container-forgot">
+                <div className="card-header h5 text-white bg-info">On ajoute des participants !</div>
                 <div className="card-body ">
                     <button className="btn btn-secondary d-flex justify-content-center"
                             style={{margin: "auto"}}>
-                        <img src={ImageLapin} height="220" width="250" alt="Account"/>
+                        <img src={ImageLapin} style={imageStyle}   alt="Account"/>
                     </button>
                     <form className='container text-start' onSubmit={handleSubmit}>
                         <div className="form-outline">Prénom
@@ -86,30 +100,24 @@ const AddUserToPige = () => {
                     </form>
                 </div>
             </div>
-            <div className="d-flex justify-content-around">
-
-
-                <div className="container mb-4 p-3 d-flex">
-                    <div className="card text-center d-flex flex-column" style={{height: "100%"}}
-                         id="container-forgot">
-                        <div className="card-header h5 text-white bg-info">Ils sont tous là?</div>
-                        <div className="card-body d-flex flex-column" style={{flex: "1"}}>
-
-                            <button className="btn btn-secondary d-flex justify-content-center mt-auto"
-                                    style={{width: "100%", margin: "auto"}}>
-                                <img src={ImageChapeauLapin} height="220" width="250" alt="Account"/>
-                            </button>
-                            {listFormInv.map((inv) => (
-                                <p>{inv.firstNameOfWantedUser} {inv.lastNameOfWantedUser}, {inv.emailWantedUser}</p>
-                            ))}
-                        </div>
-                        <button onClick={onClickCreateInvitations}>Tout le monde est là!</button>
-                    </div>
-                </div>
-
+            <div className="card text-center" style={cardStyle} id="container-forgot">
+                <div className="card-header h5 text-white bg-info"> Tout le monde est la?!</div>
+                <div className="card-body ">
+                    <button className="btn btn-secondary d-flex justify-content-center"
+                            style={{margin: "auto"}}>
+                        <img src={ImageChapeauLapin} style={imageStyle}   alt="Account"/>
+                    </button>
+                    <p>Les invités sont :</p>
+                    {listFormInv.map((inv) => (
+                        <p>{inv.firstNameOfWantedUser} {inv.lastNameOfWantedUser}, {inv.emailWantedUser}</p>
+                    ))}</div>
+                <button className="btn btn-info d-flex justify-content-center"
+                        style={{margin: "auto"}} onClick={onClickCreateInvitations}>Tout le monde est là!</button>
             </div>
-        </div>
-    );
+
+
+</div>
+);
 }
 
 export default AddUserToPige;
