ed2d86c7f1af39788da5195ba4093fd08022e6f4
cindy <cindy.bragdon@gmail.com>
2024-04-24 18:06:01 -0400

    notfound

--- frontend/src/pages/NotFound.js
+++ frontend/src/pages/NotFound.js
@@ -5,7 +5,7 @@ import Image404 from "../images/NotFound404.jpg";
 const NotFound = () => {
   return (
     <div className=' justify-content-center' style={{backgroundColor: "whitesmoke", fontFamily: "Reddit Mono", minHeight: "85vh"}}>
-        <div className='container '>
+        <div className='text-center mt-2 '>
             <h1  style={{color:"black"}}>Page Not Found, Page Introuvable</h1>
             <div>
                 <img src={Image404} className="rounded mx-auto d-block text-center " height="400" width="400" alt="..."/>
