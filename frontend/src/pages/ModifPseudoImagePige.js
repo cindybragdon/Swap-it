62cfb977d3f486f2a22517175af79f4c7b75a083
cindy <cindy.bragdon@gmail.com>
2024-04-28 10:14:47 -0400

    Fix ici et là

--- frontend/src/pages/ModifPseudoImagePige.js
+++ frontend/src/pages/ModifPseudoImagePige.js
@@ -1,6 +1,6 @@
 import React, {useState} from 'react';
 import ImageModif from "../images/bgPseudo.jpg";
-import {useLocation, useNavigate} from "react-router-dom";
+import {Link, useLocation, useNavigate} from "react-router-dom";
 import axios from "axios";
 
 
@@ -43,19 +43,16 @@ const ModifPseudoImagePige = () => {
         }
     }
     return (
-
         <div className="row justify-content-left" style={sectionStyle}>
-            <div id="modif-piges" className="card text-center card w-25 m-5  "
-                 style={{height: '440px', overflow: 'auto', fontFamily: "Reddit Mono"}}>
-                <div className="card-header h5 text-white " style={{backgroundColor: "#05521A"}}>Modifiez vos infos pour
-                    la pige {selectedUserPige.pige.pigeName}!
+            <div id="modif-piges" className="card text-center card w-25 m-5 mt-0"  // Ici, mt-1 est remplacé par mt-0 pour diminuer la marge du haut
+                 style={{height: '510px', overflow: 'auto', fontFamily: "Reddit Mono"}}>
+                <div className="card-header h5 text-white " style={{backgroundColor: "#05521A"}}>
+                    Modifiez vos infos pour la pige {selectedUserPige.pige.pigeName}!
                 </div>
                 <div className="card-body px-2">
-
                     <p className="card-text ">
                         Modifiez vos infos
                     </p>
-
                     <form onSubmit={onSubmit}>
                         <div className="text-start">
                             <label>Pseudo</label>
@@ -65,8 +62,6 @@ const ModifPseudoImagePige = () => {
                                    placeholder={"Votre pseudo"} value={pseudo}
                                    onChange={event => setPseudo(event.target.value)} required/>
                         </div>
-
-
                         <div className="text-start">
                             <label>Image (doit être un lien https vers une image)</label>
                         </div>
@@ -75,24 +70,25 @@ const ModifPseudoImagePige = () => {
                                    className="form-control my-3" placeholder={"Le lien https vers votre image : "}
                                    value={imagePige}
                                    onChange={event => setImagePige(event.target.value)}/>
-
                         </div>
-
                         <br/>
                         <div className="form-group row">
-                            <div className="col-sm-20 text-center">
+                            <div className="col-sm-20 text-center ">
                                 <button type="submit" style={{backgroundColor: "#549517"}}
                                         className="btn  w-30">Modifiez vos informations!
                                 </button>
+                                <div>
+                                    <Link to="/FAQ" className="btn mt-4"
+                                          style={{backgroundColor: '#34D1D8', color: 'white'}}>Aide FAQ - Comment insérer les URL </Link>
+                                </div>
                             </div>
                         </div>
-
                     </form>
                 </div>
             </div>
         </div>
-
     );
+
 };
 
 export default ModifPseudoImagePige
